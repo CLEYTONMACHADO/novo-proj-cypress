27e3abb9a64e3355b2aa318caba7ff85757fcf5f
CLEYTONMACHADO <cleytonmachado2010@gmail.com>
2025-03-17 16:39:27 -0300

    Logar

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+import Login from '../pages/login'
+
+const login = new Login()
+
+describe('Acessar a página de login e logar', () => {
+  it.only('Login com sucesso', () => {
+    cy.visit('/auth/login')
+    login.loginUser()
+
   })
+
+  it('Login errado', () => {
+    cy.visit('/auth/login')
+    login.loginUserFail()
+
+  })
+
+
+
+
 })
---
+++ cypress/pages/login.js
@@ -0,0 +1,30 @@
+class Login {
+    selectorList(){
+        const selector = {
+            iptUserName: "[name='username']",
+            iptUserPass: "[name='password']",
+            buttonLogin: "[type='submit']",
+            divErrorCred: ".oxd-alert--error"
+        }
+        return selector
+    }
+
+    loginUser(){
+        cy.get(this.selectorList().iptUserName).type('Admin')
+        cy.get(this.selectorList().iptUserPass).type('admin123')
+        cy.get(this.selectorList().buttonLogin).click()
+        cy.location('pathname').should('equal', '/web/index.php/dashboard/index') //identifica se foi para essa url
+        
+    }
+
+    loginUserFail(){
+        cy.get(this.selectorList().iptUserName).type('adminnn')
+        cy.get(this.selectorList().iptUserPass).type('2222222')
+        cy.get(this.selectorList().buttonLogin).click()
+        cy.get(this.selectorList().divErrorCred).should('be.visible') // verifica se a mensagem de erro
+        cy.get('body').should('contain', 'Invalid credentials') // verifica se o body tem o texto Invalid credentials
+    }
+
+
+}
+export default Login
